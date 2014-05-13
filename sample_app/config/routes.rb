41bac74f41ed603928247bd0f214de8701b461f2
Bala <sribalakumar@gmail.com>
2014-05-13 16:41:56 +0530

    Removing the changes followed from the PDF and following back the HTML tutorial (http://rails-3-2.railstutorial.org/book/static_pages#sec-static_pages)

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
