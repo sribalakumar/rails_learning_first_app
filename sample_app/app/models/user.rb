ccada6d4bb6fd860926d2cc3dbb45cc88745884b
Bala <sribalakumar@gmail.com>
2014-05-14 16:55:19 +0530

    Ending chapter 6

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
+  # below regex fails and the standard email verification Regex is 
+  # /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  # got that Regex from http://stackoverflow.com/questions/4770133/rails-regex-for-email-validation
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
