fbbf48d125ef9c1ce6e84e05e5b2cec3ebea2183
Bala <sribalakumar@gmail.com>
2014-05-15 12:00:01 +0530

    Finish sign in. That end's chapter 8 ( I am skipping the Rspec's for now)

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save :create_remember_token
+  
   validates :name, presence: true, length: { maximum: 50 }
   # below regex fails and the standard email verification Regex is 
   # /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
@@ -10,4 +12,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  private 
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
